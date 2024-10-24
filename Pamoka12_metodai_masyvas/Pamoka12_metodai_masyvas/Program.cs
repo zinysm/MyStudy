1f0adb3516bfd5c710906849a24568fa01735283
zinysm <m.zinys@inhus.eu>
2024-10-24 23:00:48 +0300

    be 2 ir 3 uzduoties

---
+++ Pamoka12_metodai_masyvas/Pamoka12_metodai_masyvas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pamoka12_metodai_masyvas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ pamoka12/pamoka12/Program.cs
@@ -0,0 +1,112 @@
+﻿//Paskaitos sablonai
+
+//int [] emptyArray = new int [10];
+//int[] arrayWithInitialValues = { 1, 2, 3, 88, 105, 999 };
+
+//Console.WriteLine(emptyArray[5]);
+
+//arrayWithInitialValues[4] = 23;
+
+//string[] sentences = { "abc", "text", "random" };
+//for (int i = 0; i < sentences.Length; i++)
+//{
+//    Console.WriteLine(sentences[i]);
+//}
+
+//for(int i = 0; i <emptyArray.Length; i++)
+//{
+//    emptyArray[i] = i * i;
+//    Console.WriteLine(emptyArray[i]);
+//}
+
+// Uzduotys 1.1
+
+
+//for (int i = 0; i < arrayWithInitialValues.Length; i++)
+//{
+//    arrayWithInitialValues[i] = arrayWithInitialValues[i] * arrayWithInitialValues[i];
+//}
+//Console.WriteLine("{" + arrayWithInitialValues[0] + "," + arrayWithInitialValues[1] + "," + arrayWithInitialValues[2] + "," + arrayWithInitialValues[3] + "," + arrayWithInitialValues[4] + "," + arrayWithInitialValues[5] + "");
+
+//int [] numbers = { 5, 2, 3, 10 , 6, 9 };
+////int [] results = Methods.RoundNumber(numbers);
+//int [] resultsReverse = Methods.Reverse(numbers);
+////int total = Methods.TotalSum(numbers);
+//int max = Methods.MaxNumber(numbers);
+////Console.WriteLine(total);
+//Console.WriteLine(max);
+//Methods.ResultPrint(resultsReverse);
+string word = "labas";
+char [] chars = Methods.CharsArray(word);
+Methods.ResultPrint(chars); 
+
+public static class Methods
+{
+    
+    public static int[] RoundNumber(int[] numbers)
+    {
+
+        int[] results = new int[numbers.Length]; 
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            results[i] = numbers[i] * numbers[i];
+           
+        }
+        return results;
+    }
+    public static void ResultPrint(char[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+    public static int TotalSum(int[] numbers)
+    {
+
+        int result = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            
+            result = result + numbers[i];
+
+        }
+        return result;
+    }
+    public static int MaxNumber(int[] numbers)
+    {
+        int result = 0;     
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > result)
+            {
+                result = numbers[i];
+            }
+                       
+
+        }
+        return result;
+    }
+    public static int[] Reverse(int[] numbers)
+    {
+        int j = numbers.Length-1;
+        int[] results = new int[numbers.Length];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+          results[i] = numbers[numbers.Length - i-1];
+        }
+        return results;
+    }
+    public static char[] CharsArray(string word)
+    {
+        char[] chars = word.ToCharArray();
+        for (int i = 0; i < chars.Length; i++)
+        {
+            chars[i] = chars[i];
+        }
+        return chars;
+    }
+}
+
+
+    
